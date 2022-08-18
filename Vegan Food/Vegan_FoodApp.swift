e132752e1ee7bef951462ed8e4ea92dfe25aa4f0
azet <ali>
2022-08-18 14:17:04 +0700

    Initial Commit

---
+++ Vegan Food/Vegan_FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vegan_FoodApp.swift
+//  Vegan Food
+//
+//  Created by Alzaen on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Vegan_FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
